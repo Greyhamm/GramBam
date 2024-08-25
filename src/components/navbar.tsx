338b3c446e3455f5ca31b3a643ea81f3f1e8bc51
Greyhamm <flokstraagraham@gmail.com>
2024-08-25 04:04:19 -0600

    Removed Grambam from navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,9 +10,6 @@ const Navbar = async () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-4">
-            <Link href="/" className="text-xl font-bold">
-              Grambam
-            </Link>
             <Link href="/" className="nav-link">
               Homepage
             </Link>
